7048bb62d5d835e1c81e8631743765eb7d2dfee1
Tien Phong <tienphong2203@gmail.com>
2025-01-26 11:01:39 +0700

    BE+FE: Create base project

---
+++ src/server/src/main/java/com/maiphong/insightcommerce/InsightcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.maiphong.insightcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightcommerceApplication.class, args);
+	}
+
+}
---
+++ src/server/src/test/java/com/maiphong/insightcommerce/InsightcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiphong.insightcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
