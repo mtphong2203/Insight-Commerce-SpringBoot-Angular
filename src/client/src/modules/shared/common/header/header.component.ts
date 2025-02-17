5ffa03cef100bc261c71d68fca5e90e5b05b2c0b
Tien Phong <tienphong2203@gmail.com>
2025-02-17 15:18:17 +0700

    FE: CSS for header with mobile screen and others

--- src/client/src/modules/shared/common/header/header.component.ts
+++ src/client/src/modules/shared/common/header/header.component.ts
@@ -1,17 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule, IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faBars, faClose } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
   public logoEShop: string = './assets/images/E-Shop.jpg';
+  public hamburger: IconDefinition = faBars;
+  public close: IconDefinition = faClose;
+  public isShow: boolean = false;
 
   public navItem: any[] = [
     { title: 'Home', link: '' },
@@ -25,5 +30,13 @@ export class HeaderComponent {
     { title: 'Login', link: '/auth/login' },
     { title: 'Register', link: '/auth/register' },
   ]
+
+  public toggleMenu(): void {
+    this.isShow = true;
+  }
+
+  public closeToggle(): void {
+    this.isShow = false;
+  }
 
 }
